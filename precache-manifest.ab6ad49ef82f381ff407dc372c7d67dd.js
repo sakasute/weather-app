feebd91336236ed85f412de2ee7bb474225972cb
Sakari Sutela <sakari.sutela@gmail.com>
2019-02-14 20:12:50 +0200

    Updates

---
+++ precache-manifest.ab6ad49ef82f381ff407dc372c7d67dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d9c99a4f19116c6d931",
+    "url": "/weather-app/static/js/runtime~main.8d9c99a4.js"
+  },
+  {
+    "revision": "9e82dba354d3509700fa",
+    "url": "/weather-app/static/js/main.9e82dba3.chunk.js"
+  },
+  {
+    "revision": "39bbbf2ccb1693fee29d",
+    "url": "/weather-app/static/js/2.39bbbf2c.chunk.js"
+  },
+  {
+    "revision": "9e82dba354d3509700fa",
+    "url": "/weather-app/static/css/main.aea2442a.chunk.css"
+  },
+  {
+    "revision": "de8378ccf6e182332df53f9b412522b9",
+    "url": "/weather-app/index.html"
+  }
+];
